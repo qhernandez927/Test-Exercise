d8f986d598aa0915a3e69c24f5b0ff50a5020218
Quinn Hernandez <Quinn@quinns-mini-3.attlocal.net>
2020-04-11 17:31:41 -0500

    one form opening and onload event added for functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// let contentForms = document.querySelectorAll('.form-content');
-const troubleFindingProduct = document.querySelector('.trouble-finding-product');
-const contactUsClick = document.getElementById('contact-us');
+window.onload = function (){
+    let contentForms = document.querySelectorAll(".form-content");
+    const troubleFindingProduct = document.querySelector('.trouble-finding-product');
+    let contactUsClick = document.getElementById("contact-us");
 
-contactUsClick.addEventListener("click", displayContentForms());
+    contactUsClick.addEventListener("click", displayContentForms);
 
-function displayContentForms() {
-    document.querySelectorAll('.form-content').style.display = "block";
-}
+    function displayContentForms() {
+        document.querySelector(".form-content").style.display = "block";
+        console.log("i got clicked");
+    }
+};
