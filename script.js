9036cff6c21a09c31e72385aa639289278a0b51c
Quinn Hernandez <Quinn@quinns-mini-3.attlocal.net>
2020-04-11 18:36:32 -0500

    independent IDs added to each form. Seperate display: none for ec form in css. functional hidden forms that open when clicked in JS

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ window.onload = function (){
     let contentForms = document.querySelectorAll(".form-content");
     const troubleFindingProduct = document.querySelector('.trouble-finding-product');
     let contactUsClick = document.getElementById("contact-us");
+    let productSuggestionClick = document.getElementById("product-suggestion");
 
-    contactUsClick.addEventListener("click", displayContentForms);
+    contactUsClick.addEventListener("click", displayContactForm);
+    productSuggestionClick.addEventListener("click", displayProductform);
 
-    function displayContentForms() {
-        document.querySelector(".form-content").style.display = "block";
-        console.log("i got clicked");
+    function displayProductform() {
+        document.querySelector(".product-form-content").style.display = "block";
+    }
+
+    function displayContactForm() {
+        document.querySelector(".contact-form-content").style.display = "block";
     }
 };
