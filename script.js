620acafeb07981c42068218c086eafb968e6affd
Quinn Hernandez <Quinn@quinns-mini-3.attlocal.net>
2020-04-12 16:28:48 -0500

    contact form swtich to product suggestion completed

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ window.onload = function (){
     let contactFormContent = document.querySelector(".contact-form-content");
     let contactReason = document.getElementById("contact-reason");
     let contactFeedback = document.getElementById("feedback-box");
-    let troubleFindingProduct = document.querySelector('.trouble-finding-product');
     let contactUsClick = document.getElementById("contact-us");
     let contactUsername = document.querySelector('input[name=contact-username]');
     let contactEmail = document.querySelector('input[name=contact-email]');
@@ -30,20 +29,42 @@ window.onload = function (){
     }
 
     function productSuggestionFormValidation() {
+        // emailValidater(productUserEmail.value);
         if(productUsername.value === '' || productUserEmail.value === '' || prodcutDescription.value === '') {
             alert("All fields are required");
             return false;
         } else {
             return true;
-        }
-    }
+        };
+    };
 
+
+    // function emailValidater(userEmail) {
+    //     let mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+
+    //     if(userEmail.match(mailFormat)){
+    //         alert("This is a valid email address")
+    //         return true;
+    //     } else {
+    //         alert("This is an invalid email address")
+    //         return false;
+    //     }
+    // }
+
+    function troubleFindingProductSwitch() {
+        if(contactReason.selectedIndex === 2){
+            displayProductform()
+            productUsername.focus();
+            alert("We noticed you're having trouble finding a product. Try using the Product Suggestion Form!");
+        };
+    };
 
     function displayContactForm() {
         contactFormContent.style.display = "block";
     }
 
     function contactFormValidation() {
+        troubleFindingProductSwitch()
         if(contactUsername.value === '' || contactEmail.value === '' || contactReason.value === '' || contactFeedback.value === '') {
             alert("All fields are required");
             return false;
